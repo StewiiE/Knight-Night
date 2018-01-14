c4ed83b9399a8f78aaa0b4218f65d9384c7bff36
Alex Stewart <Alex-Stewart13@hotmail.co.uk>
2018-01-14 19:22:54 +0000

    Started enemy AI. Enemy rotates towards and follows player.

---
+++ Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+
+    public float currentHealth;
+    float maxHealth = 100f;
+    public float damage;
+
+    private void Start()
+    {
+
+    }
+
+    void Death()
+    {
+        Destroy(gameObject);
+    }
+}
